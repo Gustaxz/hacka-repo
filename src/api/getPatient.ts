6ae0a3adf661ab21974776342cd5404be7ca79d2
Gustaxz <guxtavogusteible@gmail.com>
2023-10-09 18:25:42 -0300

    fix: resolving patients list

--- src/api/getPatient.ts
+++ src/api/getPatient.ts
@@ -1,11 +1,31 @@
-import axios from "axios"
+const patients = [
+	{
+		nome: "Gustavo",
+		senha: "123456",
+		email: "gustavo@gmail.com",
+		idade: "20",
+		cpf: "12345678910",
+		telefone: "123456789",
+		cidade: "São Paulo",
+		estado: "SP",
+		cep: "12345678",
+	},
+	{
+		nome: "João",
+		senha: "123456",
+		email: "joao@gmail.com",
+		idade: "19",
+		cpf: "1267868689",
+		telefone: "123456789",
+		cidade: "Rio de Janeiro",
+		estado: "RJ",
+		cep: "12345678",
+	},
+]
 
 export async function getPatientByName(name: string): Promise<any> {
 	const patientArray: any[] = []
-	const patientResponse = await axios.get(
-		`https://unisaude-36591e8b57bc.herokuapp.com/api/pacientes?nome=${name}`
-	)
-	const patient = patientResponse.data["content"].filter((p: any) => p.nome === name)[0]
+	const patient = patients.filter((p: any) => p.nome === name)[0]
 
 	if (!patient) {
 		return patientArray
@@ -18,7 +38,7 @@ export async function getPatientByName(name: string): Promise<any> {
 		if (key !== "senha" && key !== "endereco" && key !== "id") {
 			patientArray.push({
 				type: key[0].toUpperCase() + key.slice(1),
-				value: patient[key],
+				value: patient[key as keyof typeof patient],
 			})
 		}
 	})
